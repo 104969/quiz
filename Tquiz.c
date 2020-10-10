47a057640ca5ab30e80668c114fc67d4878a0531
JANHAVI <69165721+104969@users.noreply.github.com>
2020-10-10 11:16:25 +0530

    Add files via upload

---
+++ Tquiz.c
@@ -0,0 +1,528 @@
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+void show_record();
+void reset_score();
+void help();
+void edit_score(float , char []);
+int main()
+     {
+     int countr,r,r1,count,i,n;
+     float score;
+     char choice;
+     char playername[20];
+     mainhome:
+     system("cls");
+     printf("\t\t\tC PROGRAM TECHNO-QUIZ GAME\n");
+     printf("\n\t\t________________________________________");
+
+     printf("\n\t\t\t   WELCOME ");
+     printf("\n\t\t\t      to ");
+     printf("\n\t\t\t   THE GAME ");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t > Press S to start the game");
+     printf("\n\t\t > Press V to view the highest score  ");
+     printf("\n\t\t > Press R to reset score");
+     printf("\n\t\t > press H for help            ");
+     printf("\n\t\t > press Q to quit             ");
+     printf("\n\t\t________________________________________\n\n");
+     choice=toupper(getch());
+     if (choice=='V')
+	{
+	show_record();
+	goto mainhome;
+	}
+     else if (choice=='H')
+	{
+	help();getch();
+	goto mainhome;
+	}
+	else if (choice=='R')
+	{reset_score();
+	getch();
+	goto mainhome;}
+	else if (choice=='Q')
+	exit(1);
+    else if(choice=='S')
+    {
+     system("cls");
+
+    printf("\n\n\n\n\n\n\n\n\n\n\t\t\tResister your name:");
+     gets(playername);
+
+    system("cls");
+    printf("\n ------------------  Welcome %s to C Program Techno-Quiz Game --------------------------",playername);
+    printf("\n\n Here are some tips you might wanna know before playing:");
+    printf("\n -------------------------------------------------------------------------");
+    printf("\n >> There are 2 rounds in this Quiz Game,BASIC ROUND & CHALLANGE ROUND");
+    printf("\n >> In basic round you will be asked a total of 3 questions to test your");
+    printf("\n    general knowledge. You are eligible to play the game if you give atleast 2");
+    printf("\n    right answers, otherwise you can't proceed further to the Challenge Round.");
+    printf("\n >> Your game starts with CHALLANGE ROUND. In this round you will be asked a");
+    printf("\n    total of 10 questions. Each right answer will be awarded 100,000 points!");
+    printf("\n    By this way you can earn upto ONE MILLION game points!!!!!..........");
+    printf("\n >> You will be given 4 options and you have to press A, B ,C or D for the");
+    printf("\n    right option.");
+    printf("\n >> You will be asked questions continuously, till right answers are given");
+    printf("\n >> No negative marking for wrong answers!");
+    printf("\n\n\t!!!!!!!!!!!!! ALL THE BEST !!!!!!!!!!!!!");
+    printf("\n\n\n Press Y  to start the game!\n");
+    printf("\n Press any other key to return to the main menu!");
+    if (toupper(getch())=='Y')
+		{
+		    goto home;
+        }
+	else
+		{
+        goto mainhome;
+       system("cls");
+       }
+
+     home:
+     system("cls");
+     count=0;
+     for(i=1;i<=3;i++)
+     {
+    system("cls");
+     r1=i;
+
+
+     switch(r1)
+		{
+		case 1:
+		printf("\n\nWhich of the following is a Palindrome number?");
+		printf("\n\nA.42042\t\tB.101010\n\nC.23232\t\tD.01234");
+		if (toupper(getch())=='C')
+			{
+			    printf("\n\nCorrect!!!");count++;
+			    getch();
+			    break;
+}
+		else
+		       {
+		           printf("\n\nWrong!!! The correct answer is C.23232");
+		           getch();
+		       break;
+		       }
+
+        case 2:
+		printf("\n\n\nThe format identifier ‘%i’ is also used for which data type?");
+		printf("\n\nA.char\t\tB.int\n\nC.float\t\tD.double");
+		if (toupper(getch())=='B')
+			{printf("\n\nCorrect!!!");count++;
+			getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is B.int");
+		       getch();
+		       break;}
+
+        case 3:
+		printf("\n\n\nWhat is the precedence of arithmetic operators (from highest to lowest)?");
+		printf("\n\nA.(%, *, /, +, –)\t\tB. (%, +, /, *, –)\n\nC.(+, -, %, *, /)\t\tD.(%, +, -, *, /)");
+		if (toupper(getch())=='A')
+			{printf("\n\nCorrect!!!");count++;
+			getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is A.(%, *, /, +, –)");
+		       getch();
+		       break;}
+
+        case 4:
+		printf("\n\n\n———– was the first city in India to adopt electric traction.");
+		printf("\n\nA.delhi\t\tB.Bombay\n\nC.culkatta\t\tD.Chennai");
+		if (toupper(getch())=='B')
+			{printf("\n\nCorrect!!!");count++;
+			getch();
+			 break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is B.Bombay");
+		       getch();
+		       break;}
+
+        case 5:
+        printf("\n\n\nWhat type electric drive is used in cranes??");
+        printf("\n\nA.Group\t\tB.Mt. Multimotor.\n\nC. Individual\t\tD.Both A and C");
+        if (toupper(getch())=='B')
+               {printf("\n\nCorrect!!!");count++;
+               getch();
+                break;}
+        else
+		       {printf("\n\nWrong!!! The correct answer is B.Multimotor");
+		       getch();
+		       break;}
+
+        case 6:
+		printf("\n\n\nWhich speed control method preferred for constant torque drive?");
+		printf("\n\nA. Field control\t\tB.None of above.\n\nC. Mechanical loading system.\t\tD.Armature voltage control.");
+		if (toupper(getch())=='D' )
+			{printf("\n\nCorrect!!!");count++;
+			getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is D.Armature voltage control");
+		       getch();
+		       break;}}
+		       }
+
+	if(count>=2)
+	{goto test;}
+	else
+	{
+	system("cls");
+	printf("\n\nSORRY YOU ARE NOT ELIGIBLE TO PLAY THIS GAME, BETTER LUCK NEXT TIME");
+	getch();
+	goto mainhome;
+	}
+     test:
+     system("cls");
+     printf("\n\n\t*** CONGRATULATION %s you are eligible to play the Game ***",playername);
+     printf("\n\n\n\n\t!Press any key to Start the Game!");
+     if(toupper(getch())=='p')
+		{goto game;}
+game:
+     countr=0;
+     for(i=1;i<=10;i++)
+     {system("cls");
+     r=i;
+
+     switch(r)
+		{
+		case 1:
+		printf("\n\nIf field current is decreased in shunt dc motor, the speed of the motor");
+		printf("\n\nA.remains same\t\tB.decreases\n\nC.increases\t\tD.none of above");
+		if (toupper(getch())=='C')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;getch();}
+		else
+		       {printf("\n\nWrong!!! The correct answer is C.increases");getch();
+		       goto score;
+		       break;}
+
+		case 2:
+		printf("\n\n\nEddy current loss will depends on");
+		printf("\n\nA.Frequency\t\tB.flux density\n\nC.Thickness\t\tD.all of above");
+		if (toupper(getch())=='D')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is D.All of above");getch();
+		      goto score;
+		       break;
+		       }
+
+        case 3:
+		printf("\n\n\n In a dc machine 6 pole wave winding is used. The number of parallel paths are?? ");
+		printf("\n\nA.6\t\tB.4\n\nC.2\t\tD.1");
+		if (toupper(getch())=='C')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is C.2");getch();
+		       goto score;
+		       break;}
+
+        case 4:
+		printf("\n\n\nThe Laws of Electromagnetic Induction were given by?");
+		printf("\n\nA.Faraday\t\tB.Tesla\n\nC.Maxwell\t\tD.Coulomb");
+		if (toupper(getch())=='A')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+		       {
+                printf("\n\nWrong!!! The correct answer is A.Faraday");getch();
+		       goto score;
+		       break;
+		       }
+
+        case 5:
+		printf("\n\n\nIn what unit is electric power measured?");
+		printf("\n\nA.Coulomb\t\tB.Watt\n\nC.Power\t\tD.Units");
+		if (toupper(getch())=='B')
+			{printf("\n\nCorrect!!!");countr++;getch(); break;}
+		else
+		       {
+		           printf("\n\nWrong!!! The correct answer is B.watt");
+		       getch();
+		       goto score;
+		       break;
+		       }
+
+		case 6:
+		printf("\n\n\nTo obtain a high value of capacitance, the permittivity of dielectric medium should be?");
+		printf("\n\nA.low\t\tB.high\n\nC.unity\t\tD.zero");
+		if (toupper(getch())=='B' )
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is B.high");goto score;
+		       getch();
+		       break;}
+
+        case 7:
+		printf("\n\n\nInductor does not allow the sudden change of?");
+		printf("\n\nA.energy\t\tB.power\n\nC.voltage\t\tD.current");
+		if (toupper(getch())=='D')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is D.current");getch();
+		       goto score;
+		       break;}
+
+        case 8:
+		printf("\n\n\nInternal resistance of ideal current source is?");
+		printf("\n\nA.zero\t\tB.infinite\n\nC.one\t\tD.100 ohm");
+		if (toupper(getch())=='B')
+			{printf("\n\nCorrect!!!");countr++;getch(); break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is B.infinite");getch();
+		       goto score;
+		       break;}
+
+        case 9:
+		printf("\n\n\n A 555 IC is used to produce?");
+		printf("\n\nA.colck\t\tB.register\n\nC.count\t\tD.byte");
+		if (toupper(getch())=='A')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is A.clock");getch();
+		       goto score;
+		       break;}
+
+        case 10:
+		printf("\n\n\nBase-10 system is also called as");
+		printf("\n\nA.decimal\t\tB.binary\n\nC.tenth system\t\tD.decinary");
+		if (toupper(getch())=='A')
+			{printf("\n\nCorrect!!!");countr++;getch(); break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is A.decimal");getch();break;goto score;}
+
+        case 11:
+		printf("\n\n\n A LED produces light when?");
+		printf("\n\nA.unbiased\t\tB.reverse bias\n\nC.forward bias\t\tD.none of the above");
+		if (toupper(getch())=='C')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+              {printf("\n\nWrong!!! The correct answer is C.forward bias");getch();
+              break;goto score;}
+
+        case 12:
+		printf("\n\n\n 8085 microprocessor has how many pins");
+		printf("\n\nA.40\t\tB.39\n\nC.41\t\tD.37");
+		if (toupper(getch())=='A')
+			  {printf("\n\nCorrect!!!");countr++;getch();
+			   break;}
+		else
+              {printf("\n\nWrong!!! The correct answer is A.40");getch();
+              break;goto score;}
+
+		case 13:
+		printf("\n\n\nWhat is SIM?");
+		printf("\n\nA.Setup interrupt mask\t\tB. Select interrupt mask.\n\nC.Sorting interrupt mask.\t\tD. Set interrupt mask.");
+		if (toupper(getch())=='D')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is D. Set interrupt mask.");getch();
+		       break;goto score;}
+
+        case 14:
+		printf("\n\n\nWhat is the size of an int data type??");
+		printf("\n\nA.Depends on the system/compiler\t\tB.4 bytes\n\nC.8 bytes\t\tD.Cannot be determined");
+		if (toupper(getch())=='A')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is A.Depends on the system/compiler");getch();
+		       break;goto score;}
+
+		case 15:
+		printf("\n\n\nWhich of the following is used for generating time varying wave forms?");
+		printf("\n\nA.MOSFET\t\tB.UJT\n\nC.PIN diode\t\tD.tunnel diode");
+		if (toupper(getch())=='B')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is B.UJT");getch();
+		       goto score;
+		       break;}
+
+		case 16:
+		printf("\n\n\nWhat is the unit of measurement of distance between Stars?");
+		printf("\n\nA.Light Year\t\tB.Coulomb\n\nC.Nautical Mile\t\tD.Kilometer");
+		if (toupper(getch())=='A')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;
+			}
+		else
+		       {printf("\n\nWrong!!! The correct answer is A.Light Year");getch();
+		       goto score;
+		       break;}
+
+
+		case 17:
+		printf("\n\n\nThe units for transconductance are");
+		printf("\n\nA.SIEMENS\t\tB.OHM\n\nC.ampere\t\tD.volt");
+		if (toupper(getch())=='A')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is A.siemens");getch();goto score;
+		       break;}
+
+		case 18:
+		printf("\n\n\nWind speed is measure by__________?");
+		printf("\n\nA.Lysimeter\t\tB.Air vane\n\nC.Hydrometer\t\tD.Anemometer\n\n");
+		if (toupper(getch())=='D')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is D.Anemometer");getch();goto score;
+		       break;}
+
+		case 19:
+		printf("\n\n\nWhich materials find application in MASER?");
+		printf("\n\nA.Paramagnetic\tB.Ferromagnetic\n\nC.Diamagnetic\tD.ferrimagnetic\n\n");
+		if (toupper(getch())=='C')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is C.Diamagnetic");getch();goto score;
+		       break;}
+
+		case 20:
+		printf("\n\n\nWhich hardware was used in the First Generation Computer?");
+		printf("\n\nA.Transistor\t\tB.Valves\n\nC.I.C\t\tD.S.S.I");
+		if (toupper(getch())=='B')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is B.Valves");getch();goto score;
+		       break;}
+
+		case 21:
+		printf("\n\n\nOzone plate is being destroyed regularly because of____ ?");
+		printf("\n\nA.L.P.G\t\tB.Nitrogen\n\nC.Methane\t\tD. C.F.C");
+		if (toupper(getch())=='D')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is D. C.F.C");getch();goto score;
+		       break;}
+
+		case 22:
+		printf("\n\n\n	Piezoelectric materials serves as a source of?");
+		printf("\n\nA.microwaves\t\tB.resonant waves\n\nC.music waves\t\tD.Ultrasonic");
+		if (toupper(getch())=='D')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is D.Ultrasonic");getch();goto score;
+		       break;}
+
+		case 23:
+		printf("\n\n\n	The material which has zero temperature coefficient of resistance is");
+		printf("\n\nA.aluminium\t\tB.carbon\n\nC.manganin\t\tD.porcelin");
+		if (toupper(getch())=='C')
+			{printf("\n\nCorrect!!!");countr++; getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is C.manganin");getch();goto score;
+		       break;}}}
+	score:
+    system("cls");
+	score=(float)countr*100000;
+	if(score>0.00 && score<1000000)
+	{
+	   printf("\n\n\t\t**************** CONGRATULATION *****************");
+	     printf("\n\t You won $%.2f",score);goto go;}
+
+	 else if(score==1000000.00)
+	{
+	    printf("\n\n\n \t\t**************** CONGRATULATION ****************");
+	    printf("\n\t\t\t\t YOU ARE A WINNER!!!!!!!!!");
+	    printf("\n\t\t You SCORED %.2f",score);
+	    printf("\t\t Thank You!!");
+	}
+	 else
+{
+	 printf("\n\n\t******** SORRY YOU FAILED ********");
+	    printf("\n\t\t Thanks for your participation");
+	    printf("\n\t\t TRY AGAIN");goto go;}
+
+	go:
+	puts("\n\n Press Y if you want to play next game");
+	puts(" Press any key if you want to go main menu");
+	if (toupper(getch())=='Y')
+		goto home;
+	else
+		{
+		edit_score(score,playername);
+		goto mainhome;}}}
+
+void show_record()
+    {system("cls");
+	char name[20];
+	float scr;
+	FILE *f;
+	f=fopen("score.txt","r");
+	fscanf(f,"%s%f",&name,&scr);
+	printf("\n\n\t\t*************************************************************");
+	printf("\n\n\t\t %s has secured the Highest Score %0.2f",name,scr);
+	printf("\n\n\t\t*************************************************************");
+	fclose(f);
+	getch();}
+
+void reset_score()
+    {system("cls");
+    float sc;
+	char nm[20];
+	FILE *f;
+	f=fopen("score.txt","r+");
+	fscanf(f,"%s%f",&nm,&sc);
+	sc=0;
+	fprintf(f,"%s,%.2f",nm,sc);
+    fclose(f);}
+
+void help()
+	{system("cls");
+    printf("\n\n                              HELP");
+    printf("\n -------------------------------------------------------------------------");
+    printf("\n ......................... C program Quiz Game...........");
+    printf("\n >> There are two rounds in the game, BASE ROUND & CHALLANGE ROUND");
+    printf("\n >> In warmup round you will be asked a total of 3 questions to test your general");
+    printf("\n    knowledge. You will be eligible to play the game if you can give atleast 2");
+    printf("\n    right answers otherwise you can't play the Game...........");
+    printf("\n >> Your game starts with the CHALLANGE ROUND. In this round you will be asked");
+    printf("\n    total 10 questions each right answer will be awarded 100,000 points.");
+    printf("\n    By this way you can win upto ONE MILLION points...............");
+    printf("\n >> You will be given 4 options and you have to press A, B ,C or D for the");
+    printf("\n    right option");
+    printf("\n >> You will be asked questions continuously if you keep giving the right answers.");
+    printf("\n >> No negative marking for wrong answers");
+
+	printf("\n\n\t*********************BEST OF LUCK*********************************");}
+
+
+void edit_score(float score, char plnm[20])
+	{system("cls");
+	float sc;
+	char nm[20];
+	FILE *f;
+	f=fopen("score.txt","r");
+	fscanf(f,"%s%f",&nm,&sc);
+	if (score>=sc)
+	  { sc=score;
+	    fclose(f);
+	    f=fopen("score.txt","w");
+	    fprintf(f,"%s\n%.2f",plnm,sc);
+	    fclose(f);}}
